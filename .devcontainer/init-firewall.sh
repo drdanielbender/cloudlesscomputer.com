8d49d39fe5f77a2e7d16b7d80f85d3c286de2c96
benda <a4forgejo@13ender.de>
2026-04-10 12:48:04 +0200

    Add OpenCode to devcontainer and allow access to the OpenCode Go coding plan

--- .devcontainer/init-firewall.sh
+++ .devcontainer/init-firewall.sh
@@ -73,7 +73,8 @@ for domain in \
     "marketplace.visualstudio.com" \
     "vscode.blob.core.windows.net" \
     "update.code.visualstudio.com" \
-    "api.minimax.io"; do
+    "api.minimax.io" \
+    "opencode.ai" ; do
     echo "Resolving $domain..."
     ips=$(dig +noall +answer A "$domain" | awk '$4 == "A" {print $5}')
     if [ -z "$ips" ]; then
