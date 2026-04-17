958a383a0f2537109c1c434293677e632be08ce9
drdanielbender <a4github@13ender.de>
2026-04-17 12:58:48 -0700

    Add the languagetool url to the firewall exceptions

--- .devcontainer/init-firewall.sh
+++ .devcontainer/init-firewall.sh
@@ -74,7 +74,8 @@ for domain in \
     "vscode.blob.core.windows.net" \
     "update.code.visualstudio.com" \
     "api.minimax.io" \
-    "opencode.ai" ; do
+    "opencode.ai" \
+    "api.languagetoolplus.com" ; do
     echo "Resolving $domain..."
     ips=$(dig +noall +answer A "$domain" | awk '$4 == "A" {print $5}')
     if [ -z "$ips" ]; then
